e0406701ee7a270d7c70c7acc9f641d532160eb8
Melissa Gomes Victor <melissagomesvictor@gmail.com>
2020-11-15 01:10:16 -0300

    :ambulance: TESTE
    Comit

---
+++ Calculadora.js
@@ -0,0 +1,17 @@
+let n1 = Number(20);
+let n2 = Number(10);
+function sum(){
+    let soma = (n1 + n2);
+}
+
+function sub(){
+    //TODO: Implementar subtração
+}
+
+function mult(){
+    //TODO: Implementar multiplicação
+}
+
+function div(){
+    //TODO: Implementar divisão
+}
